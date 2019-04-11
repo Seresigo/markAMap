7199f0f10e8dddfb37eaed8b785eb1814c9abb78
Seresigo <seresigo@yandex.ru>
2019-04-11 19:26:05 +0300

    add googleAuth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,35 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+import { withStyles } from '@material-ui/core/styles'
+import { from } from 'apollo-link'
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
-};
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token
+    console.log('------------------{idToken}------------------', { idToken })
+  }
+
+  const onFailure = user => console.log('------------------user------------------', user)
+
+  return (
+    <GoogleLogin
+      clientId="156026607675-ipvimjv3k98moqhj2lg4gsp0ctd17tqm.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      isSignedIn={true}
+    />
+  )
+}
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
-};
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}
 
-export default withStyles(styles)(Login);
+export default withStyles(styles)(Login)
